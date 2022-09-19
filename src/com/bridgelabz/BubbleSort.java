e4a4f6f6e3f6acf1d1809c9cab6278a154d7ce8a
Your Name <you@example.com>
2022-09-19 12:07:48 +0530

    added:logic for Reads in integers prints them in sorted order using Bubble Sort

---
+++ src/com/bridgelabz/BubbleSort.java
@@ -0,0 +1,32 @@
+package com.bridgelabz;
+import java.util.Arrays;
+public class BubbleSort {
+    static void bubbleSort(int array[]) {
+        int size = array.length;
+        //here using for loop
+         //loop to access each array element
+        for (int i = 0; i < size - 1; i++)
+            //loop to compare array elements
+            for (int j = 0; j < size - i - 1; j++)
+             // compare two adjacent elements
+             //  change > to < to sort in descending order
+                if (array[j] > array[j + 1]) {
+                    //swapping occurs if elements
+                     // are not in the intended orde/
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+    }
+     //Here create a main method
+     //all program execute in main method
+
+    public static void main(String args[]) {
+
+        int[] data = { 10,20,-2,4,7,-9 };
+        BubbleSort.bubbleSort(data);
+
+        System.out.println("Sorted Array in Ascending Order:");
+        System.out.println(Arrays.toString(data));
+    }
+}
